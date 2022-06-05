41e463858f76ed3c1dec969cfd2f5ce015361185
Alexsandro Leal Ricardo <alexsandro.ricardo@gmail.com>
2022-06-05 10:38:13 -0300

    Vetores e Objetos

---
+++ Sintaxe Básica em JavaScript/Aula03 -Vetores e Objetos/script.js
@@ -0,0 +1,45 @@
+//o que são vetores ou arrays
+
+//como declarar uma array
+/*let array = ['string',1, true];
+console.log(array);*/
+
+//pode guardar vários tipos de dados
+let array = ['string', 1, true, ['array1'], ['array2'],['array3'],['array4']];
+/*console.log(array[3]);*/
+
+/*//forEach
+array.forEach(function(item, index){console.log(item, index)});*/
+
+/*array.push('novo item');
+console.log(array);*/
+
+/*array.pop();
+console.log(array);*/
+
+/*array.shift();
+console.log(array);*/
+
+/*array.unshift('novo item no inicio');
+console.log(array);*/
+
+/*console.log(array.indexOf(true));*/
+
+/*array.splice(0, 3);
+console.log(array);*/
+
+/*let novoArray = array.slice(0,3);
+console.log(novoArray);*/
+
+let object = {String : 'string',Number: 1, Boolean: true, array:["array"], objectInterno:{objectInterno: 'objectInterno'}};
+console.log(object);
+
+var string = object.string;
+console.log(string);
+
+var arrayInterno = object.array;
+console.log(arrayInterno);
+
+var {string, Boolean, objectInterno} = object;
+console.log(string,Boolean,objectInterno);
+
