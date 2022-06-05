83ead3a41b97ef708410e234ec65c98f2a4623b3
Alexsandro Leal Ricardo <alexsandro.ricardo@gmail.com>
2022-06-04 23:59:52 -0300

    Entendo variáveis e seus valores

---
+++ Sintaxe Básica em JavaScript/Aula02 -Entendendo variaveis e seus valores/script.js
@@ -0,0 +1,99 @@
+// tipos primitivos
+
+//boolean
+/*var vOuF = false;
+console.log(typeof(vOuF));
+
+//Number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//String
+var nome = "Alexsandro";
+console.log(typeof(nome));*/
+
+//Como declarar
+/*var variavel = "Alexsandro";
+variavel = "Leal";
+console.log(variavel);*/
+
+/*let variavel = 'Alexsandro';
+variavel = 'Ricardo';
+console.log(variavel);*/
+
+/*const constante = 'Alexsandro';
+constante = 'ricardo';
+console.log(constante);*/
+
+/*var escopoLocal = 'global';
+console.log(constante);*/
+
+/*function escopoLocal(){
+
+    let escopoLocalInterno = 'Local'
+    console.log(escopoLocalInterno);
+}
+escopoLocal();*/
+
+/*
+//  atribuição
+var atribuicao = 'Alexsandro';
+console.log(atribuicao);
+
+// comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//comparacao identica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);*/
+
+/* //adicao
+var adicao = 1 + 1;
+console.log(adicao);*/
+
+/* //subtração
+var subtracao = 2 - 1;
+console.log(subtracao;*/
+
+/* //Multiplicação
+var multiplicacao = 2 * 1;
+console.log(multiplicacao) ;*/
+
+/* //Divisão Real
+var divisao = 6 / 2;
+console.log(divisao) ;*/
+
+/* //Divisão Inteira
+var divisao = 5 % 2;
+console.log(divisao) ;*/
+
+/* //Potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao) ;*/
+
+/* //maiorQue
+var maiorQue = 5 > 2;
+console.log(maiorQue) ;*/
+
+/* //menorQue
+var menorQue = 5 < 2;
+console.log(menorQue) ;*/
+
+ /*//maior ou igual á:
+var maiorIgual = 5  >= 2;
+console.log(maiorIgual) ;*/
+
+//menor ou igual á:
+/*var menorIgual = 5  <=  2;
+console.log(menorIgual) ;*/
+
+/* var e = true && true;
+console.log(e);*/
+
+/*var ou = false || false;
+console.log(e);*/
+
+/*var nao = !true;
+console.log(nao);*/
+
